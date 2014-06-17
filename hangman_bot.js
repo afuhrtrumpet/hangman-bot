211ce68162781181de0a3e6d090e99c8e359efab
Alex Fuhr <aftrumpet@opensource.osu.edu>
2014-06-17 19:54:07 -0400

    Added support for multiple channels and cut down on lines used when drawing hangman

--- hangman_bot.js
+++ hangman_bot.js
@@ -4,13 +4,13 @@ var fs = require('fs');
 var file = process.argv[2];
 
 var config = {
-	channels: ["#osuosc-hangman"],
-	server: "irc.freenode.net",
-	botName: "Hangman-Botman"
+channels: ["#osuosc-hangman", " #bayareainterns"],
+					server: "irc.freenode.net",
+					botName: "Hangman-Botman"
 };
 
 var bot = new irc.Client(config.server, config.botName, {
-	channels: config.channels
+channels: config.channels
 });
 
 var game = false;
@@ -19,74 +19,70 @@ var completedWord = "";
 var lettersTried = [];
 var manState = 0;
 
-var chooseWord = function() {
+var chooseWord = function(channel) {
 	fs.readFile(file, function(err, data) {
-		if (err) throw err;
-		var lines = data.toString().split('\n');
-		index = Math.floor(Math.random() * (lines.length - 1));
-		console.log(lines[index]);
-		completedWord = "";
-		var words = lines[index].split(' ');
-		for (var i = 0; i < words.length; i++) {
-			completedWord += new Array(words[i].length + 1).join('_');
-			if (i < words.length - 1) {
-				completedWord += ' ';
+			if (err) throw err;
+			var lines = data.toString().split('\n');
+			index = Math.floor(Math.random() * (lines.length - 1));
+			console.log(lines[index]);
+			completedWord = "";
+			var words = lines[index].split(' ');
+			for (var i = 0; i < words.length; i++) {
+				completedWord += new Array(words[i].length + 1).join('_');
+				if (i < words.length - 1) {
+					completedWord += ' ';
+				}
 			}
-		}
-		manState = 0;
-		drawHangman();
+			manState = 0;
+			drawHangman(channel);
 
-		bot.say(config.channels[0], completedWord.split('').join(' '));
-		word = lines[index].toLowerCase();
+			bot.say(channel, completedWord.split('').join(' '));
+			word = lines[index].toLowerCase();
 
-		game = true;
-		lettersTried = [];
+			game = true;
+			lettersTried = [];
 	});
 };
 
-var drawHangman = function() {
-	bot.say(config.channels[0], "_________");
-	bot.say(config.channels[0], "|         |");
+var drawHangman = function(channel) {
+	bot.say(channel, "_________");
+	bot.say(channel, "|         |");
 	if (manState > 0) {
-		bot.say(config.channels[0], "|         0");
+		bot.say(channel, "|         0");
 	} else {
-		bot.say(config.channels[0], "|");
+		bot.say(channel, "|");
 	}
 	if (manState > 3) {
-		bot.say(config.channels[0], "|        /|\\");
+		bot.say(channel, "|        /|\\");
 	} else if (manState > 2) {
-		bot.say(config.channels[0], "|        /|");
+		bot.say(channel, "|        /|");
 	} else if (manState > 1) {
-		bot.say(config.channels[0], "|         |");
+		bot.say(channel, "|         |");
 	} else {
-		bot.say(config.channels[0], "|");
+		bot.say(channel, "|");
 	}
 	if (manState > 5) {
-		bot.say(config.channels[0], "|        / \\");
+		bot.say(channel, "|        / \\");
 	} else if (manState > 4) {
-		bot.say(config.channels[0], "|        /");
+		bot.say(channel, "|        /");
 	} else {
-		bot.say(config.channels[0], "|");
+		bot.say(channel, "|");
 	}
-	bot.say(config.channels[0], "|");
-	bot.say(config.channels[0], "|");
 }
 
-var wrongGuess = function() {
+var wrongGuess = function(channel) {
 	manState++;
-	drawHangman();
+	drawHangman(channel);
 	if (manState == 6) {
-		bot.say(config.channels[0], "You lose! The word was " + word);
+		bot.say(channel, "You lose! The word was " + word);
 		game = false;
-	} else {
-		bot.say(config.channels[0], "WROOOOOOOONNNNNG!!");
 	}
-}
+};
 
 bot.addListener("message", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 14) == '.start hangman') {
-		if (!game) chooseWord();
-		else bot.say(config.channels[0], "A game is already occurring!");
+		if (!game) chooseWord(to);
+		else bot.say(to, "A game is already occurring!");
 	}
 });
 
@@ -96,7 +92,7 @@ bot.addListener("message", function(from, to, text, message) {
 			var letter = text[7];
 			console.log(letter);
 			if (lettersTried.indexOf(letter) > -1) {
-				bot.say(config.channels[0], "This letter has already been guessed.");
+				bot.say(to, "This letter has already been guessed.");
 			} else {
 				if (word.indexOf(letter) > -1) {
 					for (var i = 0; i < word.length; i++) {
@@ -106,19 +102,19 @@ bot.addListener("message", function(from, to, text, message) {
 						}
 					}
 					if (completedWord == word) {
-						bot.say(config.channels[0], "You win! The word is " + word);
+						bot.say(to, "You win! The word is " + word);
 						game = false;	
 					} else {
-						bot.say(config.channels[0], completedWord.split('').join(' '));
+						bot.say(to, completedWord.split('').join(' '));
 					}
 				} else {
-					wrongGuess();
+					wrongGuess(to);
 				}
 				lettersTried.push(letter);
 			}
 		}
 		else  {
-			bot.say(config.channels[0], "There is no currently active game");
+			bot.say(to, "There is no currently active game");
 		}
 	}
 });
@@ -128,13 +124,13 @@ bot.addListener("message", function(from, to, text, message) {
 		if (game) {
 			var guess = text.substring(11);
 			if (guess == word) {
-				bot.say(config.channels[0], "You win! The word is " + word);
+				bot.say(to, "You win! The word is " + word);
 				game = false;
 			} else {
-				wrongGuess();
+				wrongGuess(to);
 			}
 		} else {
-			bot.say(config.channels[0], "There is no currently active game");
+		bot.say(to, "There is no currently active game");
 		}
 	}
 });
@@ -147,18 +143,18 @@ bot.addListener("message", function(from, to, text, message) {
 				letterPool = setCharAt(letterPool, letterPool.indexOf(lettersTried[i]), '_');
 			}
 		}
-		bot.say(config.channels[0], letterPool);
+		bot.say(to, letterPool);
 	}
 });
 
 bot.addListener("message", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 7) == ".status") {
-		drawHangman();
-		bot.say(config.channels[0], completedWord.split('').join(' '));
+		drawHangman(to);
+		bot.say(to, completedWord.split('').join(' '));
 	}
 });
 
 function setCharAt(str,index,chr) {
 	if(index > str.length-1) return str;
 	return str.substr(0,index) + chr + str.substr(index+1);
-}
+};

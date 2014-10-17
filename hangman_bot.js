8de199d5f5b660fdd33f72d722da68f133179ace
afuhrtrumpet@gmail.com <afuhrtrumpet@gmail.com>
2014-10-17 08:08:19 -0700

    Added new mode, but not working yet

--- hangman_bot.js
+++ hangman_bot.js
@@ -4,9 +4,9 @@ var fs = require('fs');
 var file = process.argv[2];
 
 var config = {
-channels: ["#osuosc-hangman", " #cwdg", "#hangman"],
+channels: ["#osuosc-hangman-testing"],
 					server: "irc.freenode.net",
-					botName: "Hangman-Botman"
+					botName: "Crashdummyman"
 };
 
 var bot = new irc.Client(config.server, config.botName, {
@@ -18,6 +18,7 @@ var word = {};
 var completedWord = {}; 
 var lettersTried = {};
 var manState = {};
+var waitingForUser = {};
 
 for (var i = 0; i < config.channels.length; i++) {
 	var channel = config.channels[i];
@@ -26,6 +27,7 @@ for (var i = 0; i < config.channels.length; i++) {
 	completedWord[channel] = "";
 	lettersTried[channel] = [];
 	manState[channel] = 0;
+	waitingForUser[channel] = "";
 }
 
 var chooseWord = function(channel) {
@@ -34,23 +36,27 @@ var chooseWord = function(channel) {
 			var lines = data.toString().split('\n');
 			index = Math.floor(Math.random() * (lines.length - 1));
 			console.log(lines[index]);
-			completedWord[channel] = "";
-			var words = lines[index].split(' ');
-			for (var i = 0; i < words.length; i++) {
-				completedWord[channel] += new Array(words[i].length + 1).join('_');
-				if (i < words.length - 1) {
-					completedWord[channel] += ' ';
-				}
-			}
-			manState[channel] = 0;
-			drawHangman(channel);
-
-			bot.say(channel, completedWord[channel].split('').join(' '));
-			word[channel] = lines[index].toLowerCase();
-
-			game[channel] = true;
-			lettersTried[channel] = [];
+			setWord(channel, lines[index]);
 	});
+};
+
+var setWord = function(channel, word) {
+	completedWord[channel] = "";
+	var words = word.split(' ');
+	for (var i = 0; i < words.length; i++) {
+		completedWord[channel] += new Array(words[i].length + 1).join('_');
+		if (i < words.length - 1) {
+			completedWord[channel] += ' ';
+		}
+	}
+	manState[channel] = 0;
+	drawHangman(channel);
+
+	bot.say(channel, completedWord[channel].split('').join(' '));
+	word[channel] = word.toLowerCase();
+
+	game[channel] = true;
+	lettersTried[channel] = [];
 };
 
 var drawHangman = function(channel) {
@@ -88,14 +94,36 @@ var wrongGuess = function(channel) {
 	}
 };
 
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener("message#", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 14) == '.start hangman') {
-		if (!game[to]) chooseWord(to);
-		else bot.say(to, "A game is already occurring!");
+		if (!game[to]) {
+			if (text.length < 16 || text.charAt(15) != '0' && text.charAt(15) != '1') {
+				bot.say(to, "Please choose a game type! Either .start hangman 0 or .start hangman 1.");
+			} else {
+				if (text.charAt[15] == '0') chooseWord(to);
+				else {
+					bot.say(to, from + ": please message me with a word.");
+					waitingForUser[to] = from;
+				}
+			}
+		}
+		else {
+			bot.say(to, "A game is already occurring!");
+		}
 	}
 });
 
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener("pm", function(nick, text, message) {
+	var channel;
+	for (channel in waitingForUser) {
+		if (nick == waitingForUser[channel]) {
+			setWord(channel, text);
+			waitingForUser[channel] = "";
+		}
+	}
+});
+
+bot.addListener("message#", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 7) == '.guess ') {
 		if (game[to]) {
 			var letter = text[7];
@@ -132,7 +160,7 @@ bot.addListener("message", function(from, to, text, message) {
 	}
 });
 
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener("message#", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 10) == ".guessword") {
 		if (game[to]) {
 			var guess = text.substring(11);
@@ -148,7 +176,7 @@ bot.addListener("message", function(from, to, text, message) {
 	}
 });
 
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener("message#", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 8) == ".letters") {
 		var letterPool = "abcdefghijklmnopqrstuvwxyz";
 		for (var i = 0; i < lettersTried[to].length; i++) {
@@ -160,7 +188,7 @@ bot.addListener("message", function(from, to, text, message) {
 	}
 });
 
-bot.addListener("message", function(from, to, text, message) {
+bot.addListener("message#", function(from, to, text, message) {
 	if (text.toLowerCase().substring(0, 7) == ".status") {
 		drawHangman(to);
 		bot.say(to, completedWord[to].split('').join(' '));
